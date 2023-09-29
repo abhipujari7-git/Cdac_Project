9318683cfdb5c3998e804d4d75f49a9e6fd09169
71076_Abhishek <abhishekpujari3708@gmail.com>
2023-09-29 21:53:54 +0530

    add

---
+++ Frontend/hotel-management-system-frontend-master/src/page/Footer.jsx
@@ -0,0 +1,162 @@
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div>
+      <div class="container my-5">
+        <footer class="text-center text-lg-start text-center text-color">
+          <div class="container-fluid p-4 pb-0">
+            <section class="">
+              <div class="row">
+                <div class="col-lg-12 col-md-12 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-center text-color">
+                    Hotel Booking System
+                  </h5>
+
+                  <p class=" text-center text-color">
+                    
+                  Experience a seamless blend of convenience and freedom. Our dedicated staff is at your service around the clock, ensuring that every need, no matter the hour, is met with a smile. Your comfort knows no boundaries.
+
+At Hotel Nest, we believe that time is the ultimate luxury. Our 24/7 accessibility isn't just an amenity; it's a philosophy that celebrates the beauty of a life well-lived, unrestricted by the constraints of a clock. Embrace the moments that matter, whenever they may arise.
+<br></br>
+With us, time is your canvas, and your experiences are timeless.
+<br></br>
+
+Welcome to a world where time bends to your will.
+                  </p>
+                </div>
+
+                {/* <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">About us</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 3
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 4
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">Contact us</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 3
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 4
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">Careers</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 3
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 4
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">Links</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 3
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 4
+                      </a>
+                    </li>
+                  </ul>
+                </div> */}
+              </div>
+            </section>
+
+            <hr class="mb-4" />
+
+            <section class="">
+              <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3 text-color">Register for free</span>
+                <Link to="/user/customer/register" class="active">
+                  <button
+                    type="button"
+                    class="btn btn-outline-light btn-rounded bg-info text-color"
+                  >
+                    Sign up!
+                  </button>
+                </Link>
+              </p>
+            </section>
+
+            <hr class="mb-4" />
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
